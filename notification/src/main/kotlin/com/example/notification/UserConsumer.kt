91e77fae26461275d7e328b385954ff6f05a5316
jerry.hj <jerry.hj@kakaostyle.com>
2022-12-27 01:43:33 +0900

    feat: notification, coupon 로직은 userEvent에 따라 작동

---
+++ notification/src/main/kotlin/com/example/notification/UserConsumer.kt
@@ -0,0 +1,37 @@
+package com.example.notification
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Service
+
+@Service
+class UserConsumer {
+
+    private val log = LoggerFactory.getLogger(this::class.simpleName)
+    private val objectMapper = jacksonObjectMapper()
+
+    @KafkaListener(
+        topics = ["user.v1"],
+        concurrency = "1",
+    )
+    fun consumerListen(records: List<ConsumerRecord<String, String>>) {
+        val userEvents = records.map { objectMapper.readValue(it.value(), UserEvent::class.java) }
+        log.info(userEvents.toString())
+        userEvents.forEach {
+            if (it.action == Action.CREATE) {
+                log.info("userId: ${it.id} 생성 알림")
+            }
+        }
+    }
+}
+
+data class UserEvent(
+    val id: Long,
+    val action: Action
+)
+
+enum class Action {
+    CREATE, UPDATE
+}

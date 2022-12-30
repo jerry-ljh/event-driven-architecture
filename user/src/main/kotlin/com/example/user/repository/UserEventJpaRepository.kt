a4de7c2f8372cea19df5a96c0662d8e4b10243bb
jerry.hj <jerry.hj@kakaostyle.com>
2022-12-30 14:45:16 +0900

    test: user저장로직 테스트

--- user/src/main/kotlin/com/example/user/repository/UserEventJpaRepository.kt
+++ user/src/main/kotlin/com/example/user/repository/UserEventJpaRepository.kt
@@ -3,4 +3,6 @@ package com.example.user.repository
 import com.example.user.domain.UserEvent
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserEventJpaRepository: JpaRepository<UserEvent, Long>
+interface UserEventJpaRepository : JpaRepository<UserEvent, Long> {
+    fun findByEvent(event: Map<String, Any>): UserEvent?
+}
